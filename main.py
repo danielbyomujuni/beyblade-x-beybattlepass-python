ad18d5f2b95edaea29572f037eb3db47ef01ef8d
Daniel <danielbyomujuni@gmail.com>
2024-07-24 22:58:01 -0400

    socket info

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import binascii
 from bleak import BleakClient
 import logging
 import discover
+import socket
 
 logger = logging.getLogger(__name__)
 
 device_address = "da:c4:51:02:25:b7"
 
 def callback(sender, data):
-    print(f"[{sender}]: {data}")
+    print(f"[{sender}]: {binascii.hexlify(data)}")
 
 async def main():
     address = await discover.scan_battle_pass();
@@ -17,29 +18,20 @@ async def main():
     if (address == None):
         return;
 
-    async with BleakClient(address) as client:
-        print("connected to battlepass")
+    client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM);
+    client.connect((address, 4))
+    print("connected to battlepass")
+    try:
+        while True:
+            message = bytes.fromhex('0240000900050004001213000100')
+            client.send(message);
+            data = client.recv(1024)
+    except:
+        print("error")
 
-        for service in client.services:
-            print(f"[Service] {service}")
-            for characteristics in service.characteristics:
-                print(f"    [Characteristic] {characteristics}")
 
 
-        #print(f"\n[Characteristic] {client.services.get_characteristic(15)}")
-        #start notify
-        await client.start_notify(17, callback)
 
-        await client.write_gatt_char(client.services.get_characteristic(15).uuid, bytes.fromhex('51'));
-
-        print("scanning")
-
-        for i in range(0, 16 * 8):
-            print(i)
-            await client.write_gatt_char(client.services.get_characteristic(15).uuid, i.to_bytes());
-            await asyncio.sleep(0.5)
-
-        await client.stop_notify(client.services.get_characteristic(17))
-        print("disconnected from battlepass")
+    print("disconnected from battlepass")
 
 asyncio.run(main())
